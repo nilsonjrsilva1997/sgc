52d8c7479911753573e96ea3e811b17434bf4f8b
Nilson Junior <nilsonjrsilva97@gmail.com>
2021-07-14 05:09:34 -0300

    project

---
+++ src/main/java/br/com/sgc/SgcApplication.java
@@ -0,0 +1,11 @@
+package br.com.sgc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgcApplication {
+
+
+
+}
